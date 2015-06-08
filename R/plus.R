bcbbacafbcb481ca4d5c89d3ee3b84b908904a41
APUnatto <acmetal74@gmail.com>
2015-06-08 21:08:29 +0900

    message

---
+++ R/plus.R
@@ -0,0 +1,14 @@
+#' A Plus Function
+#'
+#' This function allows you to add two numbers.
+#' @param x First Value. Defaults to 0.
+#' @param y Second Value. Defaults to 0.
+#' @keywords plus
+#' @export
+#' @examples
+#' plusFunc(4,5) gives you 9
+plusFunc <- function(x=0,y=0) {
+  z = 0
+  z = x + y
+  return(z)
+}
